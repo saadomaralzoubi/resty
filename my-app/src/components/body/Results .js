4ca6bd226f98e71639836744005930e092f3618a
saadalzoubi <saad.alzoubi.1994@gmail.com>
2022-05-07 19:20:20 +0300

    add

--- my-app/src/components/body/Results .js
+++ my-app/src/components/body/Results .js
@@ -8,6 +8,8 @@ class Results extends React.Component {
   render() {
     return (
       <div className="result">
+        <p>Method: {this.props.method}</p>
+        <p> URL: {this.props.url}</p>
         <br></br>
         <section>{<JSONPretty data={this.props.data}></JSONPretty>}</section>
       </div>
